73afdf75a1f680c3d698d800ea0451ad945d4caf
Termux Tools Hub <termuxtoolshub@gmail.com>
2025-08-03 13:49:00 +0600

    run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-curl -sL https://raw.githubusercontent.com/termuxtoolshub/operator-lookup/main/secret.txt | base64 -d > temp.sh
-bash temp.sh
-rm temp.sh
+# Download the secret base64 file and decode to script
+curl -sL https://raw.githubusercontent.com/termuxtoolshub/operator-lookup/main/secret.txt | base64 -d > lookup.sh
+
+# Run the decoded script interactively
+bash lookup.sh
+
+# Delete temp file
+rm lookup.sh
